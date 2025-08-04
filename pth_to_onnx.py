dfba45a5298192c4f705fe96643640e696c7498e
Shivanib-001 <128892374+Shivanib-001@users.noreply.github.com>
2025-08-04 12:58:47 +0530

    Update pth_to_onnx.py

--- pth_to_onnx.py
+++ pth_to_onnx.py
@@ -1,7 +1,7 @@
 import torch
 import argparse
 
-from yolact import Yolact
+from yolact_onnx import Yolact
 from utils.functions import SavePath
 from utils.augmentations import FastBaseTransform
 from data import cfg, set_cfg
@@ -13,25 +13,30 @@ def parse_args(argv=None):
                         type = str,
                         help = 'Trained state_dict file path to convert.'
                         )
-
+    parser.add_argument('--config', default=None,
+                        help='The config object to use.')
+                        
     return parser.parse_args(argv)
 
 if __name__ == '__main__':
     args = parse_args()
 
-    model_path = SavePath.from_str(args.trained_model)
-
-    # TODO (from the original eval.py): Bad practice? Probably want to do a name lookup instead.
-    args.config = model_path.model_name + '_config'
-    print('Config not specified. Parsed %s from the file name.\n' % args.config)
-    set_cfg(args.config)
+    if args.config is not None:
+        set_cfg(args.config)      
+    if args.config is None:
+        model_path = SavePath.from_str(args.trained_model)
+        args.config = model_path.model_name + '_config'
+        print('Config not specified. Parsed %s from the file name.\n' % args.config)
+        set_cfg(args.config)
 
     with torch.no_grad():
         net = Yolact()
         net.load_weights(args.trained_model)
         net.eval()
 
-        # If I not feedforward a signal into the network the exported onnx give very obscure results and I have no idea why.
         pred_outs = net(FastBaseTransform()(torch.randn(1, 550, 550,3)))
 
-        torch.onnx.export(net, torch.autograd.Variable(torch.randn(1, 3, 550, 550)), args.trained_model.replace('.pth', '.onnx'), verbose=True)
+        torch.onnx.export(net, torch.autograd.Variable(torch.randn(1, 3, 550, 550)), args.trained_model.replace('.pth', '.onnx'), verbose=True,input_names=['input'],
+                  output_names=['loc', 'conf', 'mask', 'priors', 'proto'],
+                  opset_version=11)
+        
